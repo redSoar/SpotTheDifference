93477c750b3d178e0383bee77f5e03c31e978b5f
redSoar <132908031+redSoar@users.noreply.github.com>
2023-12-03 11:09:04 -0500

    holy poop
    what is all this stuff bro i just added the CImg.h

--- cmake-build-debug/CMakeFiles/SpotTheDifference.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/SpotTheDifference.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/c/Users/ninte/CLionProjects/SpotTheDifference/main.cpp" "CMakeFiles/SpotTheDifference.dir/main.cpp.o" "gcc" "CMakeFiles/SpotTheDifference.dir/main.cpp.o.d"
+  "/mnt/c/Users/Hyper/OneDrive/Documents/GitHub/SpotTheDifference/main.cpp" "CMakeFiles/SpotTheDifference.dir/main.cpp.o" "gcc" "CMakeFiles/SpotTheDifference.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
